1c281a5658024e3463eb2a9d07f48c221ca97327
Will Anderson <WillAnderson1339@gmail.com>
2025-03-26 09:59:22 -0700

    On my own - added the guesses coding

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -8,12 +8,13 @@ import '../css/Main.css'
 
 function Main() {
     const [currentWord, setCurrentWord] = useState('react')
+    const [guesses, setGuesses] = useState(generateGuesses())
+    const [attempts, setAttempts] = useState(8)
 
     const languageElements = languages.map((language, index) => {
         const styles = {
             backgroundColor: language.backgroundColor,
             color: language.color,
-            // border: "1px solid green"
         }
 
         return (
@@ -27,6 +28,61 @@ function Main() {
         )
     })
 
+    const guessElements = guesses.map((guess, index) => {
+        return (
+            // <span 
+            //     className={guess.right ? "guess guess-right" : guess.wrong ? "guess guess-wrong" : "guess guess-normal"} 
+            //     // className="guess-right"  
+            //     key={index}
+            //     >
+            //     {guess.letter.toUpperCase()}
+            // </span>
+                <button 
+                    // className="guess" 
+                    className={guess.right ? "guess guess-right" : guess.wrong ? "guess guess-wrong" : "guess guess-normal"} 
+                    key={index} 
+                    onClick={() => handleGuess(guess.letter)}
+                    >
+                    {guess.letter.toUpperCase()}
+                </button>
+            )
+    })
+
+    function generateGuesses() {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        const alphabetArray = alphabet.split("")
+        const guessArray = []
+        const guessElements = alphabetArray.map((letter, index) => {
+            const guess = {
+                id: index,
+                letter: letter, 
+                right: false, 
+                wrong: false
+            }
+            guessArray.push(guess)
+        })
+
+        return guessArray
+    }
+
+    function isInWord(letter) {
+        return currentWord.includes(letter)
+    }
+
+    function handleGuess(letter) {
+        // console.log(letter)
+        let attribute = "default"
+        isInWord(letter) ? attribute = "right" : attribute = "wrong"
+
+        setGuesses(prevGuessArray =>
+            prevGuessArray.map(guess =>
+              guess.letter === letter
+                ? { ...guess, [attribute]: true }     // the [] is called computed property name and will be replaced with value of name
+                : guess
+            )
+          );
+    }
+
     return (
         <>
             <main>
@@ -39,12 +95,14 @@ function Main() {
                     <p>Well done! 🎉</p>
                 </section>
                 <section className="language-chips">
-                    {/* <span style={{backgroundColor: '#E2680F', color: '#00FF00'}}>FOO</span> */}
-                    {/* <span>BAR</span> */}
                     {languageElements}
                 </section>
                 <section className="word-spaces">
                     {letterElements}
+                </section>
+                <section className="guesses">
+                    {/* <h2>Guesses</h2> */}
+                    {guessElements}
                 </section>
             </main>
         </>
